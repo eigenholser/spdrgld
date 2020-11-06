c88cb754ca94b85ca707b772320fb246b4360b4f
Scott Overholser <eigenholser@users.noreply.github.com>
2020-11-05 19:55:39 -0700

    Replace logging with log4j2. Tweek default properties for credentials.

--- src/main/java/com/eigenholser/outrider/spdrgld/SpdrGldApplication.java
+++ src/main/java/com/eigenholser/outrider/spdrgld/SpdrGldApplication.java
@@ -1,12 +1,16 @@
 package com.eigenholser.outrider.spdrgld;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpdrGldApplication {
+	private static final Logger logger = LogManager.getLogger(SpdrGldApplication.class);
 
 	public static void main(String[] args) {
+		logger.debug("***** Startup *****");
 		SpringApplication.run(SpdrGldApplication.class, args);
 	}
 
--- src/main/java/com/eigenholser/outrider/spdrgld/SpdrGldController.java
+++ src/main/java/com/eigenholser/outrider/spdrgld/SpdrGldController.java
@@ -2,18 +2,22 @@ package com.eigenholser.outrider.spdrgld;
 
 import java.util.Date;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SpdrGldController {
+	private static final Logger logger = LogManager.getLogger(SpdrGldController.class);
+	
 	@Autowired
 	SpdrGldRepository repo;
 	
@@ -23,8 +27,13 @@ public class SpdrGldController {
 	}
 	
 	@PostMapping("/spdrgld")
-	@ResponseStatus(HttpStatus.CREATED)
-	public SpdrGld addRow(@RequestBody SpdrGld request) {
-		return repo.save(request);
+	public ResponseEntity<SpdrGld> addRow(@RequestBody SpdrGld request) {
+		logger.debug("Request: " + request);
+		try {
+			return new ResponseEntity<SpdrGld>(repo.save(request), HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<SpdrGld>(request, HttpStatus.BAD_REQUEST);
+		}
+		
 	}
 }
