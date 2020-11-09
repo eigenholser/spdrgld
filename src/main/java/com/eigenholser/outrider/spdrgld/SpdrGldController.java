a2c738cbef7dce455a69602cf0fd9a5c60c5d913
Ry Overholser <overry@isu.edu>
2020-11-09 14:16:06 -0700

    Added default values and parameter checking 	modified:   src/main/java/com/eigenholser/outrider/spdrgld/SpdrGldController.java 	modified:   src/main/java/com/eigenholser/outrider/spdrgld/SpdrGldService.java 	modified:   src/main/resources/application.properties

--- src/main/java/com/eigenholser/outrider/spdrgld/SpdrGldController.java
+++ src/main/java/com/eigenholser/outrider/spdrgld/SpdrGldController.java
@@ -2,6 +2,7 @@ package com.eigenholser.outrider.spdrgld;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -16,10 +17,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class SpdrGldController {
 	private static final Logger logger = LogManager.getLogger(SpdrGldController.class);
+	private static final int MAX_PAGE_SIZE = 50;
 
 	@Autowired
 	SpdrGldService spdrGldSvc;
@@ -30,10 +33,19 @@ public class SpdrGldController {
 	}
 
 	@GetMapping("/spdrgld/{startDate}/{endDate}")
-	public Page<SpdrGld> getRowByDateBetween(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
+	public ResponseEntity<Page<SpdrGld>> getRowByDateBetween(
+			@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
 			@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate endDate,
-			@RequestParam("pageSize") int pageSize, @RequestParam("pageNum") int pageNum) {
-		return spdrGldSvc.getSpdrGldDateRange(startDate, endDate, pageSize, pageNum);
+			@RequestParam(name = "pageSize") Optional<Integer> pageSize,
+			@RequestParam(name = "pageNum") Optional<Integer> pageNum,
+			@RequestParam(name = "sortOrder") Optional<String> sortOrder) {
+		try {
+			return new ResponseEntity<Page<SpdrGld>>(
+					spdrGldSvc.getSpdrGldDateRange(startDate, endDate, pageSize, pageNum, sortOrder), HttpStatus.OK);
+		} catch (IllegalArgumentException e) {
+			logger.warn("exception: " + e.getMessage());
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Something bad happened");
+		}
 	}
 
 	@PostMapping("/spdrgld")
--- src/main/java/com/eigenholser/outrider/spdrgld/SpdrGldService.java
+++ src/main/java/com/eigenholser/outrider/spdrgld/SpdrGldService.java
@@ -1,7 +1,10 @@
 package com.eigenholser.outrider.spdrgld;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -10,6 +13,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SpdrGldService {
+	private static final Logger logger = LogManager.getLogger(SpdrGldService.class);
+	private static final Integer MAX_PAGE_SIZE = 50;
+	private static final Integer DEFAULT_PAGE_NUM = 0;
+	private static final String SORT_ASC = "ASC";
+	private static final String SORT_DESC = "DESC";
 
 	@Autowired
 	SpdrGldRepository repo;
@@ -18,9 +26,44 @@ public class SpdrGldService {
 		return repo.findByDate(date);
 	}
 
-	public Page<SpdrGld> getSpdrGldDateRange(LocalDate startDate, LocalDate endDate, int pageSize, int pageNum) {
+	public Page<SpdrGld> getSpdrGldDateRange(LocalDate startDate, LocalDate endDate, Optional<Integer> optPageSize,
+			Optional<Integer> optPageNum, Optional<String> optSortOrder) {
+
+		Integer pageSize = MAX_PAGE_SIZE;
+		Integer pageNum = DEFAULT_PAGE_NUM;
+		String sortOrder = SORT_ASC; //default sort order
+
+		if (optSortOrder.isPresent()) {
+			sortOrder = optSortOrder.get().toUpperCase();
+		}
+
+		if (optPageSize.isPresent()) {
+			pageSize = optPageSize.get();
+		}
+
+		if (optPageNum.isPresent()) {
+			pageNum = optPageNum.get() - 1;
+		}
+		
+		if(!sortOrder.equals(SORT_ASC) && !sortOrder.equals(SORT_DESC)) {
+			throw new IllegalArgumentException("Invalid sort order!");
+		}
+
+		if (pageSize.compareTo(MAX_PAGE_SIZE) == 1 || pageSize.equals(0)) {
+			pageSize = MAX_PAGE_SIZE;
+		}
+
+		if (pageSize.compareTo(0) == -1) {
+			throw new IllegalArgumentException("Invalid page size!");
+		}
+
+		if (pageNum.compareTo(DEFAULT_PAGE_NUM) == -1) {
+			throw new IllegalArgumentException("Invalid page number!");
+		}
+
 		return repo.findByDateBetween(startDate, endDate, //
-				PageRequest.of(pageNum - 1, pageSize, Sort.by(Sort.Direction.ASC, "date")));
+				PageRequest.of(pageNum, pageSize,
+						Sort.by(sortOrder.equals(SORT_ASC) ? Sort.Direction.ASC : Sort.Direction.DESC, "date")));
 	}
 
 	public SpdrGld save(SpdrGld spdrGld) {
